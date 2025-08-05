40f7d14e41ebb7efa1c9d859a94e7502b7262223
jun17183 <jun17183@naver.com>
2025-08-06 01:39:08 +0900

    feat. 알림 타입 상수 처리

--- src/basic/shared/types.ts
+++ src/basic/shared/types.ts
@@ -34,7 +34,7 @@ export interface Coupon {
 export interface Notification {
   id: string;
   message: string;
-  type: 'error' | 'success' | 'warning';
+  type: NotificationType;
 }
 
 export type AppState = ReturnType<typeof useAppState>;
@@ -48,6 +48,10 @@ export const ACTIVE_TAB_PRODUCTS = 'products';
 export const ACTIVE_TAB_COUPONS = 'coupons';
 export const DISCOUNT_TYPE_AMOUNT = 'amount';
 export const DISCOUNT_TYPE_PERCENTAGE = 'percentage';
+export const NOTIFICATION_TYPE_ERROR = 'error';
+export const NOTIFICATION_TYPE_SUCCESS = 'success';
+export const NOTIFICATION_TYPE_WARNING = 'warning';
 
 export type ActiveTab = typeof ACTIVE_TAB_PRODUCTS | typeof ACTIVE_TAB_COUPONS;
 export type DiscountType = typeof DISCOUNT_TYPE_AMOUNT | typeof DISCOUNT_TYPE_PERCENTAGE;
+export type NotificationType = typeof NOTIFICATION_TYPE_ERROR | typeof NOTIFICATION_TYPE_SUCCESS | typeof NOTIFICATION_TYPE_WARNING;
