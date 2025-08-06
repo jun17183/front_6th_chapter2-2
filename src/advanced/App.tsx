19e233faf7a95e63dfa080044f45d93c20115dc7
jun17183 <jun17183@naver.com>
2025-08-07 02:57:42 +0900

    fix. Provider 제거거

--- src/advanced/App.tsx
+++ src/advanced/App.tsx
@@ -3,27 +3,25 @@ import { AdminPage } from './admin';
 import { UserPage } from './user';
 import { Notifications } from './shared/components/Notifications';
 import { Header } from './shared/components/Header';
-import { Provider, useAtom } from 'jotai';
+import { useAtom } from 'jotai';
 import { isAdminAtom } from './store/atoms/appAtom';
 
 const App = () => {
   const [isAdmin] = useAtom(isAdminAtom);
 
   return (
-    <Provider>
-      <div className="min-h-screen bg-gray-50">
-        {/* 알림 */}
-        <Notifications />
+    <div className="min-h-screen bg-gray-50">
+      {/* 알림 */}
+      <Notifications />
 
-        {/* 헤더 */}
-        <Header />
+      {/* 헤더 */}
+      <Header />
 
-        {/* 메인 컨텐츠 */}
-        <main className="max-w-7xl mx-auto px-4 py-8">
-          {isAdmin ? <AdminPage /> : <UserPage />}
-        </main>
-      </div>
-    </Provider>
+      {/* 메인 컨텐츠 */}
+      <main className="max-w-7xl mx-auto px-4 py-8">
+        {isAdmin ? <AdminPage /> : <UserPage />}
+      </main>
+    </div>
   );
 };
 
