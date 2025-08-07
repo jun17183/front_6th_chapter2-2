11ec448a4d863d50dc1cf298a6adcafbeb45735b
jun17183 <jun17183@naver.com>
2025-08-07 21:41:40 +0900

    build. 배포 박치기 중

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   base: '/front_6th_chapter2-2/',
+  plugins: [react()],
   build: {
     rollupOptions: {
       input: 'index.html'
     }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts']
   }
 })
