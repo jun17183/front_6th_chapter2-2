f7fbe301754e2ff60071e4d9dbba0d124b44754d
jun17183 <jun17183@naver.com>
2025-08-07 21:35:41 +0900

    build. 배포 박치기 중

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // @ts-ignore
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
-  },
   base: '/front_6th_chapter2-2/',
   build: {
     rollupOptions: {
